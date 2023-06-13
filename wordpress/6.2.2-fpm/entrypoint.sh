31870efa7a82178c24571355836f4fd406dc0361
purplegrape <purplegrape4@gmail.com>
2023-06-13 09:45:29 +0800

    update

--- wordpress/6.2.2-fpm/entrypoint.sh
+++ wordpress/6.2.2-fpm/entrypoint.sh
@@ -3,11 +3,11 @@
 set -e -x
 
 if [ ! -d /data/wordpress ];then
-  unzip -q -n -d /data /wordpress-6.2.2.zip
+  unzip -q -n -d /data /usr/src/wordpress-6.2.2.zip
   chown -R www-data.www-data /data/wordpress/wp-content
+  ln -sf /data/wordpress /var/www/html
 fi
 
-/usr/sbin/apache2ctl -M
 /usr/bin/php -m
 
 exec $@
